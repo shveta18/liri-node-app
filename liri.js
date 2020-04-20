6f11aeea94a9941c8c8fc726e32af4ae1f0dd87f
Shveta Kumar <sk@shveta.ca>
2020-04-20 18:31:26 -0400

    spotify artist and track returned

--- liri.js
+++ liri.js
@@ -9,12 +9,12 @@ var app = process.argv[2];
 var searchField = "";
 var text = "";
 
-for (var i = 3; i<process.argv.length; i++) {
-     text += " " + process.argv[i];
+for (var i = 3; i < process.argv.length; i++) {
+    text += " " + process.argv[i];
     console.log("Search criteria was: " + text);
 }
-searchField = text; 
-console.log("COMPLETE field" + searchField); 
+searchField = text;
+console.log("COMPLETE field" + searchField);
 
 
 switch (app) {
@@ -38,40 +38,55 @@ switch (app) {
 function spotify() {
 
     var Spotify = require('node-spotify-api');
- 
+
     var spotify = new Spotify(keys.spotify);
-     
-    spotify.search({ type: 'track', query: searchField }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
-     
-    console.log(JSON.stringify(data)); 
-    });
+
+    spotify
+        .search({ type: 'track', query: searchField })
+        .then(function (response) {
+            console.log(response.tracks.items.length);
+
+            for (var i = 0; i < response.tracks.items.length; i++) {
+
+                console.log("-----------------------" + searchField + " track result#" + (i + 1) + "------------------------------");
+
+                for (var j = 0; j < response.tracks.items[i].album.artists.length; j++) {
+                    console.log("Artist: " + response.tracks.items[i].album.artists[j].name);
+                    console.log("Song: " + response.tracks.items[i].name);
+
+                }
+
+
+
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 };
 
 function concert() {
     axios.get("https://rest.bandsintown.com/v4/artists/" + searchField + "/events?app_id=codingbootcamp").then(
         function (response) {
-            
+
             console.log(response.data.length);
 
-            for (var i = 0; i<response.data.length; i++) {
-                console.log("-----------------------" + searchField + " event#"+ (i+1) + "------------------------------");
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("-----------------------" + searchField + " event#" + (i + 1) + "------------------------------");
                 console.log("Name of Venue: " + response.data[i].venue.city + "," + response.data[i].venue.country);
                 console.log("Location of Venue: " + response.data[i].venue.location);
                 console.log("Date of Event: " + response.data[i].datetime);
-                
+
 
             }
-        
+
         })
         .catch(function (error) {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 console.log("---------------Data---------------");
-                
+
             } else if (error.request) {
                 // The request was made but no response was received
                 // `error.request` is an object that comes back with details pertaining to the error that occurred.
